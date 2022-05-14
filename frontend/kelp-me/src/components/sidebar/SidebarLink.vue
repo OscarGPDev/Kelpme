c76e27dac1db2503c8287db2cc84a56fe9fddb66
Jorge Márquez <jmarquezl1200@alumno.ipn.mx>
2022-05-14 12:47:03 -0500

    No sirve el sidebar

--- frontend/kelp-me/src/components/sidebar/SidebarLink.vue
+++ frontend/kelp-me/src/components/sidebar/SidebarLink.vue
@@ -3,28 +3,28 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { collapsed } from './state'
 
-export default({
-    props:{
-        to: {type:String, required: true},
-        icon: {type:String, required: true}
-    },
-    setup(props) {
-        const route = useRoute()
-        const isActive = computed(() => route.path == props.to)
-        return { isActive, collapsed}
-    }
-})
+export default {
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String, required: true }
+  },
+  setup(props) {
+    const route = useRoute()
+    const isActive = computed(() => route.path === props.to)
+    return { isActive, collapsed }
+  }
+}
 </script>
 
 <template>
-    <router-link :to="to" class="link" :class="{ active: isActive }">
+  <router-link :to="to" class="link" :class="{ active: isActive }">
     <i class="icon" :class="icon" />
     <transition name="fade">
-    <span v-if="!collapsed">
+      <span v-if="!collapsed">
         <slot />
-    </span>
+      </span>
     </transition>
-    </router-link>
+  </router-link>
 </template>
 
 <style scoped>
@@ -55,6 +55,7 @@ export default({
   color: white;
   text-decoration: none;
 }
+
 .link:hover {
   background-color: var(--sidebar-item-hover);
 }
--- frontend/kelp-me/src/main.js
+++ frontend/kelp-me/src/main.js
@@ -3,8 +3,4 @@ import App from './App.vue'
 import router from './router'
 import '@fortawesome/fontawesome-free/js/all'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App).use(router).mount('#app')
