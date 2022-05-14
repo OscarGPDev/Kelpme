0c0090b64954ef701100c7907c62e452fed33cd3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-14 17:33:59 -0600

    Creando archivo formulario para organizaciones

---
+++ frontend/kelp-me/src/views/FormularioView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    hoaaaa
+  </div>
+  <form action="">
+    name: <input type="text" v-model="name" class="form-control">
+
+  </form>
+
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
