879c8f565027c8a9cfdd247130fb388c36bef27f
Oscar Guzman <oscarprograb@gmail.com>
2022-05-14 13:17:31 -0500

    correccion para el funcionamiento de dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv');
 const app = express()
 
 
+dotenv.config();
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
 
