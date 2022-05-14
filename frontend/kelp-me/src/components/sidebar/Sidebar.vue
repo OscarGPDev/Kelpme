b13ce53941316b29dffbed1c2c69fca6a70af032
Jorge Márquez <jmarquezl1200@alumno.ipn.mx>
2022-05-14 09:24:07 -0500

    Pasando la flecha hasta abajo

--- frontend/kelp-me/src/components/sidebar/Sidebar.vue
+++ frontend/kelp-me/src/components/sidebar/Sidebar.vue
@@ -18,29 +18,35 @@ export default {
 
 <style>
     :root{
-    --sidebar-bg-color: #2f855a;
-    --sidebar-item-hover: #38a169;
-    --sidebar-item-active: #276749;
+        --sidebar-bg-color: #2f855a;
+        --sidebar-item-hover: #38a169;
+        --sidebar-item-active: #276749;
     }
 </style>
 
 
 <style scoped>
     .sidebar {
-    color: white;
-    background-color: var(--sidebar-bg-color);
+        color: white;
+        background-color: var(--sidebar-bg-color);
 
-    float: left;
-    position: fixed;
-    z-index: 1;
-    top:0;
-    left: 0;
-    bottom: 0;
-    padding: 0.5em;
+        float: left;
+        position: fixed;
+        z-index: 1;
+        top:0;
+        left: 0;
+        bottom: 0;
+        padding: 0.5em;
 
-    transition: 0.3s ease;
+        transition: 0.3s ease;
 
-    display: flex;
-    flex-direction: column; 
+        display: flex;
+        flex-direction: column; 
+    }
+
+    .collapse-icon {
+        position: absolute;
+        bottom: 0;
+        padding: 0.75em;
     }
 </style>
