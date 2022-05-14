a37ef6775417f4298a403f56395b4a5ed8a08eff
Jorge Márquez <jmarquezl1200@alumno.ipn.mx>
2022-05-14 09:04:29 -0500

    Creando componente Sidebar

---
+++ frontend/kelp-me/src/components/sidebar/Sidebar.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+   props:{},
+    setup() {
+        
+    },
+}
+</script>
+
+<template>
+    <div class="sidebar"></div>
+</template>
+
+<style>
+:root{
+ --sidebar-bg-color: #2f855a;
+ --sidebar-item-hover: #38a169;
+ --sidebar-item-active: #276749;
+}
+</style>
