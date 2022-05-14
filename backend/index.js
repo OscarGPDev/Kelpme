1a491d1e68479400a60a9cf720a8f4dfc6d1c83b
Carlos Contreras <cecontrerasc@outlook.com>
2022-05-14 16:28:00 -0500

    feature: endpoint para peticiones get de noticias, endpoint dummy para agregar noticias

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const {getOrganizations} = require("./Organizacion/organizacion");
-const app = express()
+const { getNews, addNews } = require("./Noticia/noticia");
+const app = express();
 
 
 dotenv.config();
 app.get('/ObtenerOrganizaciones', getOrganizations);
+app.get('/Noticias', getNews);
+app.post('/Noticias', addNews);
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`);
