3984e0538b0cb3ecb1e1dc581dcd8a155033b1da
JaviMx <javimxmine@hotmail.com>
2022-05-14 12:41:23 -0500

    Segunda propuesta de pagina inicial, elegida

--- frontend/kelp-me/src/components/Inicial.vue
+++ frontend/kelp-me/src/components/Inicial.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="d-flex flex-column justify-content-center align-items-center">
         <h1>Sargazo</h1>
-        <h2>¿Bueno o malo?</h2>
+        <h2 class="mb-5">¿Bueno o malo?</h2>
 
-        <button class="btn btn-outline-primary mt-3"> Quiero saber más </button>
+        <button class="btn btn-outline-blue mt-3"> Quiero saber más </button>
     </section>
 </template>
 
@@ -20,17 +20,29 @@ export default {
         left: 0;
         width: 100%;
         height: 100vh;
-        background-image: url('../assets/img/WorldOceansDay-bro.svg');
+        background-image: url('../assets/img/algas.jpg');
         background-repeat: no-repeat;
-        background-size: contain;
+        background-size: cover;
         background-position: center;
         margin: 0 auto;
-        background-color:  #22c1c3;
         
     }
 
     h1, h2{
-        color: #3D80CC;
-        font-size: 3rem;
+        color: #fff;
+        font-size: 4rem;
     }
+
+    .btn-outline-blue{
+        background-color: rgb(123, 179, 179);
+        color: white;
+        padding: 1rem 2rem;
+        border: 1px solid rgb(146, 146, 146);
+        border-radius: .5rem;
+    }
+
+    .btn-outline-blue:hover{
+        background-color: rgb(141, 202, 202);
+    }
+
 </style>
