2a03ba98b0492008108fe0c868610191a88e0dc1
Jorge Márquez <jmarquezl1200@alumno.ipn.mx>
2022-05-14 09:09:28 -0500

    Posicionado Sidebar a la izquierda

--- frontend/kelp-me/src/components/sidebar/Sidebar.vue
+++ frontend/kelp-me/src/components/sidebar/Sidebar.vue
@@ -12,9 +12,30 @@ export default {
 </template>
 
 <style>
-:root{
- --sidebar-bg-color: #2f855a;
- --sidebar-item-hover: #38a169;
- --sidebar-item-active: #276749;
-}
+    :root{
+    --sidebar-bg-color: #2f855a;
+    --sidebar-item-hover: #38a169;
+    --sidebar-item-active: #276749;
+    }
+</style>
+
+
+<style scoped>
+    .sidebar {
+    color: white;
+    background-color: var(--sidebar-bg-color);
+
+    float: left;
+    position: fixed;
+    z-index: 1;
+    top:0;
+    left: 0;
+    bottom: 0;
+    padding: 0.5em;
+
+    transition: 0.3s ease;
+
+    display: flex;
+    flex-direction: column; 
+    }
 </style>
