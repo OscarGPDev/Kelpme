2a3d63480a2e1b7251764875bf2362c6b3fd7ffd
JaviMx <javimxmine@hotmail.com>
2022-05-14 14:46:22 -0500

    Pagina inicial agregada

---
+++ frontend/kelp-me/src/components/home/Noticia.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Noticias</h2>
+        <h3>Titulo noticia</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta quam itaque temporibus quisquam minima, quibusdam blanditiis harum earum cupiditate nisi, similique neque nam in iste quae ullam! Nesciunt, harum laborum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, cumque molestiae non voluptate debitis voluptatem deserunt quisquam, aperiam nobis ex aliquam corporis dolore iusto error nostrum nesciunt, nam tenetur excepturi.</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/kelp-me/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <Inicial/>
+    <Noticia/>
+</template>
+
+<script>
+export default {
+    components:{
+        Inicial: defineAsyncComponent ( () => import('../components/Inicial.vue')),
+        Noticia: defineAsyncComponent ( () => import('../components/Noticia.vue')),
+    }
+}
+</script>
+
+<style>
+
+</style>
