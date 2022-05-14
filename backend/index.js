515e8326e0e77cae863b57b57338e1ce37b1918f
Oscar Guzman <oscarprograb@gmail.com>
2022-05-14 13:12:34 -0500

    cambios en el back para la utilización de prisma y express

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
+
