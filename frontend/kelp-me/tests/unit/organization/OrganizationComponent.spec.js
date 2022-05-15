991808c91d7809a88f09cae42fc14967cff9aea1
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-15 16:46:31 -0600

    Agregando prueba para OrganizationComponent

--- frontend/kelp-me/tests/unit/organization/OrganizationComponent.spec.js
+++ frontend/kelp-me/tests/unit/organization/OrganizationComponent.spec.js
@@ -1,9 +1,17 @@
 import { mount } from '@vue/test-utils';
 import OrganizationComponent from '../../../src/components/organization/OrganizationComponent.vue';
 
-describe('Test para NoticiaComponent', () => {
+describe('Test para OrganizationComponent', () => {
     it('Test 1: Pintando props', () => {
-        
+        const wrapper = mount(OrganizationComponent, {
+            props: {
+                name: 'Nivea',
+                description: 'Empresa dedicada al cuidado...'
+            }
+        });
+
+        expect(wrapper.html()).toContain('Nivea')
+        expect(wrapper.html()).toContain('Empresa dedicada al cuidado...')
        
      
     })
