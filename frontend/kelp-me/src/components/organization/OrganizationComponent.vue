a7bf364a46a1e5c1f67762efd207a28215f6cf20
Oscar Guzman <oscarprograb@gmail.com>
2022-05-14 22:44:29 -0500

    renderizado de organizacion

---
+++ frontend/kelp-me/src/components/organization/OrganizationComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        <h3>{{ name }}</h3>
+        <p>{{ description }}</p>
+      </div>
+      <div class="col-4">
+        <img alt="organization logo" v-bind:src="image"/>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    description: String,
+    image: String,
+
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 10px
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- frontend/kelp-me/src/views/OrganizacionesView.vue
+++ frontend/kelp-me/src/views/OrganizacionesView.vue
@@ -2,27 +2,53 @@
   <div class="tittle">
     Organizaciones
   </div>
- 
-        <router-link to="/formulario"> 
-          ¿Quieres registrar una organización?
-        </router-link> 
 
-
+  <router-link to="/formulario">
+    ¿Quieres registrar una organización?
+  </router-link>
+<OrganizationComponent v-for="org in this.organizaciones" :name="org.nombre" :key="org.id" :description="org.descripcion" :image="org.imagen" />
 
 </template>
 
 <script>
+import OrganizationComponent from "@/components/organization/OrganizationComponent";
+export default {
+  components: {OrganizationComponent},
+  data: () => ({
+    organizaciones: []
+  }),
+  setup() {
 
+  },
+  created() {
+    fetch(`${process.env.VUE_APP_KELP_ME_API_URL}/ObtenerOrganizaciones`,{method: "GET",}).then(res => {
+      // a non-200 response code
+      if (!res.ok) {
+        // create error instance with HTTP status text
+        console.log("erro")
+      }
+
+      return res.json();
+    })
+        .then(json => {
+          // set the response data
+          console.log(json)
+          this.organizaciones = json;
+          console.log(this.organizaciones);
+        })
+  }
+
+};
 </script>
 
 <style>
 
-.tittle{
+.tittle {
   text-align: center;
   font-size: 40px;
   margin-top: 20px;
   font-weight: 700;
-  color:#1C8388;
+  color: #1C8388;
 }
 </style>
 
