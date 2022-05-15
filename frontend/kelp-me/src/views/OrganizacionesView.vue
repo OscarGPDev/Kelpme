e6e6f54fa66c27a9d110a600d4331048b987ac2f
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-14 20:47:33 -0600

    Modificando link hacia formulario

--- frontend/kelp-me/src/views/OrganizacionesView.vue
+++ frontend/kelp-me/src/views/OrganizacionesView.vue
@@ -2,10 +2,13 @@
   <div class="tittle">
     Organizaciones
   </div>
+  <div class="toform">
+    <router-link to="/formulario"> 
+      ¿Quieres registrar una organización?
+    </router-link> 
+  </div>
  
-        <router-link to="/formulario"> 
-          ¿Quieres registrar una organización?
-        </router-link> 
+        
 
 
 
@@ -15,7 +18,7 @@
 
 </script>
 
-<style>
+<style scoped>
 
 .tittle{
   text-align: center;
@@ -23,6 +26,22 @@
   margin-top: 20px;
   font-weight: 700;
   color:#1C8388;
+}
+.toform{
+  width: 90%;
+  text-align: end;
+
+}
+a{
+  color: #1C8388;
+  text-decoration: none;
+  transition: color 0.8s ease-in-out, box-shadow 0.8s ease-in-out;
+}
+a:hover{
+  background-color: #1C8388;
+  color: white;
+  border-radius: 5px;
+  transition: color 0.8s ease-in-out, box-shadow 0.8s ease-in-out;
 }
 </style>
 
